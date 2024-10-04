022da048b1b62e189e86f8305de26b2c1c54630d
Samantha <samantha.sotoalejos@gmail.com>
2024-10-04 09:15:13 -0700

    /users GET route

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -3,6 +3,35 @@ import express from "express";
 
 const app = express();
 const port = 8000;
+const users = {
+  users_list: [
+    {
+      id: "xyz789",
+      name: "Charlie",
+      job: "Janitor"
+    },
+    {
+      id: "abc123",
+      name: "Mac",
+      job: "Bouncer"
+    },
+    {
+      id: "ppp222",
+      name: "Mac",
+      job: "Professor"
+    },
+    {
+      id: "yat999",
+      name: "Dee",
+      job: "Aspring actress"
+    },
+    {
+      id: "zap555",
+      name: "Dennis",
+      job: "Bartender"
+    }
+  ]
+};
 
 app.use(express.json());
 
@@ -14,4 +43,9 @@ app.listen(port, () => {
   console.log(
     `Example app listening at http://localhost:${port}`
   );
+});
+
+
+app.get("/users", (req, res) => {
+  res.send(users);
 });
