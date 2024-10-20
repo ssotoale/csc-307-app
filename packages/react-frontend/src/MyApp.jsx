0ff2d0ff13535bc96166f32a318d0afa6148b0e6
Samantha <samantha.sotoalejos@gmail.com>
2024-10-20 15:42:38 -0700

    create POST calls to add a user in frontend to backend

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -47,12 +47,35 @@ const characters = [
         });
         setCharacters(updated);
       }
-    function updateList(person) {
-        setCharacters([...characters, person]);
+    // function updateList(person) {
+    //     setCharacters([...characters, person]);
+    // }
+
+    // updateList users postUser post request function
+    function updateList(person) { 
+      postUser(person)
+        .then(() => setCharacters([...characters, person]))
+        .catch((error) => {
+          console.log(error);
+        })
     }
+
     // assignment 3
     function fetchUsers() {
       const promise = fetch("http://localhost:8000/users");
+      return promise;
+    }
+
+    //  make POST request and return response
+    function postUser(person) {
+      const promise = fetch("Http://localhost:8000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(person),
+      });
+  
       return promise;
     }
 
